4ca65483d3dc708dcc66f068ed94d55c60836210
Macbook <macbook@MacBook-Air-de-Macbook.local>
2025-11-07 15:22:48 +0100

    Initial watermelon 3D

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,4 @@ export default defineConfig({
 
 
 
-git init
-git add .
-git commit -m "Initial watermelon 3D"
-git branch -M main
-git remote add origin https://github.com/Mathias-Hoffmann/watermelon-ghpages.git
-git push -u origin main
+
